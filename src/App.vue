771a8b5f7baa3b926fb285be0294986420cc090c
chandrakanthvemakoti <chanduvemakoti@gmail.com>
2020-04-07 17:45:12 +0530

    modified

--- src/App.vue
+++ src/App.vue
@@ -55,7 +55,7 @@
             
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-                    <label for="male">
+                    <label for="male">Gender
                         <input
                                 type="radio"
                                 id="male"
